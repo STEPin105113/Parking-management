f90733d478929f2ad07c4274b11b8e08437fac11
STEPin105113 <69115366+STEPin105113@users.noreply.github.com>
2020-10-09 12:18:05 +0530

    Update test_parking_operation.c

--- 03_Implementation/test/test_parking_operation.c
+++ 03_Implementation/test/test_parking_operation.c
@@ -14,7 +14,7 @@ int main() {
  if (CUE_SUCCESS != CU_initialize_registry())
     return CU_get_error();
  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
- CU_add_test(suite, "login", test_login);
+ CU_add_test(suite, "login", testlogin);
   
 
 
